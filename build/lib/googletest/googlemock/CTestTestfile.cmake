8eab6ccdd5265296dda30ba64b38b795dab15150
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-06-24 23:44:15 -0400

    remove build directory

--- build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gabeiglio/Desktop/Developer/Mar/lib/googletest/googlemock
-# Build directory: /Users/gabeiglio/Desktop/Developer/Mar/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/src/CMakeFiles/mar_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmar_lib.a"
-)
