109f31da8edcc170d0654493a5fbd00930409439
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-07-09 15:05:58 -0400

    Fix up

--- test/main.cpp
+++ test/main.cpp
@@ -23,6 +23,28 @@ TEST(LexerTest, tokens) {
 
 }
 
+TEST(LexerTest, constTokens) {
+    
+    //Test code
+    char* code = "const isBool bool = false";
+
+    //Init Lexer and tokenize
+    Lexer lex(code);
+    std::vector<Token> tokens = lex.tokenize();
+
+    //Expected output
+    std::vector<Token> expected = {Token{TokenType::constKey, "const"},Token{TokenType::identifier, "isBool"}, 
+        Token{TokenType::boolType, "bool"}, Token{TokenType::equal, "="}, Token{TokenType::boolLiteral, "false"}};
+
+    //Assert vectors size first
+    EXPECT_TRUE(tokens.size() == expected.size());
+
+    //Compare types and lexemes
+    for (unsigned int i; i < tokens.size(); i++)
+        EXPECT_TRUE(tokens[i].type == expected[i].type && tokens[i].lexeme == expected[i].lexeme);
+        
+}
+
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
